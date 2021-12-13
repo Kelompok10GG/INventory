fa2a5c231aaf4d5bc4d271b6acebca9dcbb84085
C-Yonzz <panipermana10@students.amikom.ac.id>
2021-12-13 20:10:45 +0700

    Add project files.

---
+++ Inventory Management/MVVM/View Model/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Inventory_Management.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management.MVVM.View_Model
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
